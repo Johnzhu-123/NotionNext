4368977247ed68748b8bcaee54a6c0e3950bca05
Johnzhu-123 <87792450+Johnzhu-123@users.noreply.github.com>
2025-02-07 17:04:41 +0800

    Update [[...index]].js

--- pages/auth/sign-in/[[...index]].js
+++ pages/auth/sign-in/[[...index]].js
@@ -1,12 +1,14 @@
-// pages/auth/sign-in/[[...index]].js
+// pages/auth/sign-in.js
 import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
-    <SignIn
-      path="/auth/sign-in"
-      routing="path"
-      signInUrl="/auth/sign-in"
-    />
+    <div className="flex items-center justify-center min-h-screen">
+      <SignIn
+        path="/auth/sign-in"
+        routing="path"
+        signInUrl="/auth/sign-in"
+      />
+    </div>
   )
 }
