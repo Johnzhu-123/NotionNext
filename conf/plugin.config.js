f41ca72e9b021da7c96430e4db402fee5ca4932e
Johnzhu-123 <87792450+Johnzhu-123@users.noreply.github.com>
2025-02-12 17:49:26 +0800

    Update plugin.config.js

--- conf/plugin.config.js
+++ conf/plugin.config.js
@@ -23,7 +23,7 @@ module.exports = {
     'https://jsd.onmicrosoft.cn/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css',
   TianliGPT_JS:
     process.env.NEXT_PUBLIC_TIANLI_GPT_JS ||
-    '<https://jsd.cdn.zzko.cn/gh/Johnzhu-123/Post-Abstract-AI-NotionNext@master/pastking_gpt.js',
+    'https://jsd.cdn.zzko.cn/gh/Johnzhu-123/Post-Abstract-AI-NotionNext@master/pastking_gpt.js',
   TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || 'SeeYjys',
 
   // 邮件
