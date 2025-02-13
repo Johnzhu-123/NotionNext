6eccaf93ec4d48d68f7029a14b66a97663b8db6b
Johnzhu-123 <87792450+Johnzhu-123@users.noreply.github.com>
2025-02-13 11:56:17 +0800

    Add files via upload

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -2,15 +2,15 @@ const CONFIG = {
   HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示，若为false则只显示一列
   HEO_LOADING_COVER: true, // 页面加载的遮罩动画
 
-  HEO_HOME_BANNER_ENABLE: false,
+  HEO_HOME_BANNER_ENABLE: true,
 
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  // HEO_NOTICE_BAR: [
-  //  { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-  //  { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  // ],
+  HEO_NOTICE_BAR: [
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -18,24 +18,24 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '为后来人铺路',
-  HEO_HERO_TITLE_2: '为奋进者存温',
-  HEO_HERO_TITLE_3: 'seeyjys.eu.org',
-  HEO_HERO_TITLE_4: 'LINUX DO',
-  HEO_HERO_TITLE_5: '我们的目标是星辰大海',
-  HEO_HERO_TITLE_LINK: 'https://linux.do',
+  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/科研' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -58,30 +58,30 @@ const CONFIG = {
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
-   {
-     title_1: 'AfterEffect',
-     img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-     color_1: '#989bf8',
-     title_2: 'Sketch',
-     img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-     color_2: '#ffffff'
-   },
-   {
-     title_1: 'Docker',
-     img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-     color_1: '#57b6e6',
-     title_2: 'Photoshop',
-    img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-     color_2: '#4082c3'
-   },
-   {
-     title_1: 'FinalCutPro',
-     img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
-     color_1: '#ffffff',
-     title_2: 'Python',
-     img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
-     color_2: '#ffffff'
-   },
+    {
+      title_1: 'AfterEffect',
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+      color_1: '#989bf8',
+      title_2: 'Sketch',
+      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Docker',
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      color_1: '#57b6e6',
+      title_2: 'Photoshop',
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      color_2: '#4082c3'
+    },
+    {
+      title_1: 'FinalCutPro',
+      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      color_1: '#ffffff',
+      title_2: 'Python',
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      color_2: '#ffffff'
+    },
     {
       title_1: 'Swift',
       img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://linux.do',
+  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
@@ -144,7 +144,7 @@ const CONFIG = {
   HEO_POST_LIST_PREVIEW: false, // 读取文章预览
   HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
-  HEO_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
+  HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
   HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
